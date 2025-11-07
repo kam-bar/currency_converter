7c5490f54ced75b06a80ae6cb7e384cbc31c8b3c
Kamil <kam.barabasz@gmail.com>
2025-11-07 23:32:35 +0100

    Add functions

--- js/script.js
+++ js/script.js
@@ -5,30 +5,41 @@ const amountElement = document.querySelector(".js-amount");
 const currencyElement = document.querySelector(".js-currency");
 const resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const amount = +amountElement.value;
-    const currency = currencyElement.value;
-
-    let result;
+const calculateResult = (amount, currency) => {
     const rateEUR = 4.28;
     const rateGBP = 4.85;
     const rateUSD = 3.69;
 
     switch (currency) {
         case "EUR":
-            result = amount / rateEUR;
-            break;
+            return amount / rateEUR;
         case "GBP":
-            result = amount / rateGBP;
-            break;
+            return amount / rateGBP;
         case "USD":
-            result = amount / rateUSD;
-            break;
+            return amount / rateUSD;
         default:
-            alert("Nieznana waluta!");
-            return;
+            return null;
     }
+};
+
+const updateResult = (result, currency) => {
     resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-});
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    if (result === null) {
+        alert("Nieznana waluta!");
+        return;
+    }
+
+    updateResult(result, currency);
+};
+
+formElement.addEventListener("submit", onFormSubmit);
